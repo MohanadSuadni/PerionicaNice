40c1dea1cd90e1eea5a42b37e5075916f122b6c0
Mohanad Suadni <mohanadsuadni@gmail.com>
2026-04-26 23:46:05 +0200

    for SEO sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.perionica-vesa-nice.rs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.perionica-vesa-nice.rs/usluge",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.perionica-vesa-nice.rs/pricing",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.perionica-vesa-nice.rs/faq",
+      lastModified: new Date(),
+    },
+  ];
+}
